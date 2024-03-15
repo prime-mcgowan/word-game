011132a3e11b48f6af70a7f321536975d893d800
Laura McGowan <prime.lmcg@gmail.com>
2024-03-14 19:36:18 -0500

    global variables created

---
+++ js/app.js
@@ -0,0 +1,10 @@
+//Global Variables
+const guessedLetters = document.querySelector(".guessed-letters");
+const guessButton = document.querySelector(".guess");
+const textInput = document.querySelector(".letter");
+const wordInProgress = document.querySelector(".word-in-progress");
+const remainingGuesses = document.querySelector(".remaining");
+const remainingSpan = document.querySelector("span");
+const playAgainButton = document.querySelector("play-again hide");
+
+const word = "magnolia";
