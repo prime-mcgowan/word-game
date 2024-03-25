d7c544df7d903c9e0b96c7fe39d1e2e67ca068b7
Laura McGowan <prime.lmcg@gmail.com>
2024-03-25 15:21:28 -0500

    checkInput function working - messages appearing to user

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ const wordInProgress = document.querySelector(".word-in-progress");
 const remainingGuesses = document.querySelector(".remaining");
 const remainingSpan = document.querySelector("span");
 const playAgainButton = document.querySelector("play-again hide");
+const message = document.querySelector(".message");
 
 const word = "magnolia";
 
@@ -21,14 +22,28 @@ const placeholder = function (word) {
 
 placeholder(word);
 
-// selectUserNumber.addEventListener("change", function (e) {
-//   const numUsers = e.target.value;
-//   fetchData(numUsers);
-// });
-
 guessButton.addEventListener("click", function (e) {
   e.preventDefault();
+  message.innerText = "";
   const guess = letterInput.value;
-  console.log(guess);
+  //   console.log(guess);
+
+  const goodGuess = checkInput(guess);
+  if (goodGuess) {
+    makeGuess(guess);
+  }
   letterInput.value = "";
 });
+
+const checkInput = function (input) {
+  const acceptedLetter = /[a-zA-Z]/;
+  if (input.length === 0) {
+    message.innerText = `Please enter a letter`;
+  } else if (input.length > 1) {
+    message.innerText = `Please enter a single letter`;
+  } else if (!input.match(acceptedLetter)) {
+    message.innerText = `Please enter a letter from A-Z`;
+  } else {
+    return input;
+  }
+};
