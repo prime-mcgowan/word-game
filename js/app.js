cc4e84ad617d633dd5017fd35115399b5ea0c1fa
Laura McGowan <prime.lmcg@gmail.com>
2024-03-26 13:02:31 -0500

    code fully commented out

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-//Global Variables
+// * Global Variables *******************************************************************************
+// **************************************************************************************************
 const guessedLettersElement = document.querySelector(".guessed-letters");
 const guessButton = document.querySelector(".guess");
 const letterInput = document.querySelector(".letter");
@@ -9,10 +10,14 @@ const playAgainButton = document.querySelector(".play-again");
 const message = document.querySelector(".message");
 
 let word = "magnolia";
+// will contain the letters guessed by the player
 let guessedLetters = [];
 let remainingGuesses = 10;
+//ensures that the remainingGuesses # will be loaded upon page initialization
 remainingGuessesSpan.innerText = `${remainingGuesses} guesses`;
 
+// * Async functiont to get word *********************************************************************
+// ***************************************************************************************************
 const getWord = async function () {
   const response = await fetch(
     "https://gist.githubusercontent.com/skillcrush-curriculum/7061f1d4d3d5bfe47efbfbcfe42bf57e/raw/5ffc447694486e7dea686f34a6c085ae371b43fe/words.txt"
@@ -27,7 +32,8 @@ const getWord = async function () {
 
 getWord();
 
-//Function to add placeholders for each letter
+// * Function to add placeholders for each letter *****************************************************
+// ****************************************************************************************************
 const placeholder = function (word) {
   const placeholderLetters = [];
   for (let letter of word) {
@@ -39,9 +45,13 @@ const placeholder = function (word) {
 
 placeholder(word);
 
+// * Event listener for the guess button *************************************************************
+// ****************************************************************************************************
 guessButton.addEventListener("click", function (e) {
+  // prevents the page from constantly reloading
   e.preventDefault();
   message.innerText = "";
+  // variable to capture the input value
   const guess = letterInput.value;
   //   console.log(guess);
 
@@ -52,35 +62,48 @@ guessButton.addEventListener("click", function (e) {
   letterInput.value = "";
 });
 
+// * Check player's input *****************************************************
+// ****************************************************************************************************
 const checkInput = function (input) {
   const acceptedLetter = /[a-zA-Z]/;
+  // check to see if the input empty
   if (input.length === 0) {
     message.innerText = `Please enter a letter`;
+    //  check to see if the input is more than one letter
   } else if (input.length > 1) {
     message.innerText = `Please enter a single letter`;
+    // check to see if a character that does not match the pattern has been entered
   } else if (!input.match(acceptedLetter)) {
     message.innerText = `Please enter a letter from A-Z`;
   } else {
+    // if the user inputted a letter...great!! return the letter
     return input;
   }
 };
 
+// * Capture player's input **************************************************************************
+// ****************************************************************************************************
 const makeGuess = function (guess) {
+  // all letters converted to uppercase
   guess = guess.toUpperCase();
+  // check to see if the player already guessed that same letter
   if (guessedLetters.includes(guess)) {
     message.innerText = `That letter has already been guessed`;
+    //if the letter hasn't already been guessed then add it to the guessedLetters array
   } else guessedLetters.push(guess);
   console.log(guessedLetters);
 
-  numGuessesRemaining(guess);
-
-  // call this function displays the guessed letters on the screen for the user
+  // call this function to display the guessed letters on the screen for the user:
   guessedLettersUpdate();
 
+  numGuessesRemaining(guess);
+
+  // call this function to display the updated progress of the word
   updateWordProgress(guessedLetters);
 };
 
-// * Create a function to show the guessed letters
+// * Create a function to show the guessed letters - This function gets called in makeGuess ***********
+// ****************************************************************************************************
 const guessedLettersUpdate = function () {
   // empty the innerHTML of the unordered list
   guessedLettersElement.innerHTML = "";
@@ -92,7 +115,8 @@ const guessedLettersUpdate = function () {
   }
 };
 
-// * Create a function to update the word in progress & guessedLetters array is a parameter
+// * Create a function to update the word in progress & guessedLetters array is a parameter  - This gets called in makeGuess ***********
+// *************************************************************************************************************************************
 const updateWordProgress = function (guessedLetters) {
   let wordUpper = word.toUpperCase();
   // splits the word string into an array so the letter can appear in the guessedLetters array
@@ -107,11 +131,13 @@ const updateWordProgress = function (guessedLetters) {
       revealWord.push("●");
     }
   }
+  // the revealWord array letter(s) needs to join the wordInProgress so it can be updated
   wordInProgress.innerText = revealWord.join("");
   checkIfWin();
 };
 
-// * Create a function to count guesses remaining
+// * Create a function to count guesses remaining *****************************************************
+// ****************************************************************************************************
 const numGuessesRemaining = function (guess) {
   const upperWord = word.toUpperCase();
   if (!upperWord.includes(guess)) {
@@ -132,7 +158,8 @@ const numGuessesRemaining = function (guess) {
   }
 };
 
-// * Create a function to see if the player won
+// * Create a function to see if the player won - this gets called in updateWordProgress ************
+// **************************************************************************************************
 const checkIfWin = function () {
   if (word.toUpperCase() === wordInProgress.innerText) {
     message.classList.add("win");
@@ -143,7 +170,8 @@ const checkIfWin = function () {
   }
 };
 
-// * Hide and show elements
+// * Hide and show elements @ end of game *************************************************************
+// ****************************************************************************************************
 const startOver = function () {
   guessButton.classList.add("hide");
   numberOfGuessesLeft.classList.add("hide");
@@ -151,7 +179,8 @@ const startOver = function () {
   playAgainButton.classList.remove("hide");
 };
 
-// * Add click event to the play again button
+// * Add click event to the play again button *****************************************************
+// ************************************************************************************************
 playAgainButton.addEventListener("click", function () {
   message.classList.remove("win");
   guessedLetters = [];
